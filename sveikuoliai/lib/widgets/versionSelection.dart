45455fe9a99eac5abb3dff4dfbd26fbfe0470997
Rugile Rankauskaite <159070970+rankauskaite@users.noreply.github.com>
2025-04-01 00:05:22 +0300

    Rugile r programavimas (#21)
    * Ikelti nuotaiku paveiksliukai ir atvaizduoti programeleje
    
    * Patobulintas iprocio ekrano vizualas, itrauktas augalas ir daugiau informacijos apie iproti. Susieti naujo iprocio ir augalo sujungimai sukuriant iproti.
    
    * Pataisytas procentu paisymas kartu su progreso apskritimu.
    
    * Sukurtas augaliuku augimo funkcionalumas, iprocio progreso zymejimas
    
    * Padarytas iprocio salinimas
    
    * Padarytas tiklo vaizdavimas, uzduociu pridejimas ir vykdymas, tikslo istrynimas
    
    * Warningu patobulinimai
    
    * Prideta premium versijos reklama
    
    * Panaudotas programeles logotipas vizualiai kad butu galima atpazinti programele
    
    * Patvarkytas meditacijos ekrano vizualas
    
    * .
    
    * Vartotojo profilio informacijos atnaujinimas
    
    * Pataisyta

---
+++ sveikuoliai/lib/widgets/versionSelection.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class VersionSelection extends StatefulWidget {
+  final String currentVersion;
+  final Function(String) onVersionChanged;
+
+  const VersionSelection(
+      {Key? key, required this.currentVersion, required this.onVersionChanged})
+      : super(key: key);
+
+  @override
+  _VersionSelectionState createState() => _VersionSelectionState();
+}
+
+class _VersionSelectionState extends State<VersionSelection> {
+  late String selectedVersion;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedVersion = widget.currentVersion;
+    print('Selected version: $selectedVersion');
+  }
+
+  void _changeVersion(String newVersion) {
+    setState(() {
+      selectedVersion = newVersion;
+    });
+    widget.onVersionChanged(newVersion);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildVersionButton('Nemokama', '0€', selectedVersion == 'nemokama',
+            Color(0xFF72ddf7), 'free'),
+        const SizedBox(width: 10),
+        _buildVersionButton('Premium', '9.99€', selectedVersion == 'premium',
+            Color(0xFFB388EB), 'premium'),
+      ],
+    );
+  }
+
+  Widget _buildVersionButton(String label, String price, bool isSelected,
+      Color color, String version) {
+    return GestureDetector(
+      onTap: () => _changeVersion(version),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+              color: isSelected ? color.withOpacity(0.2) : Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: color, width: 2),
+              boxShadow: isSelected
+                  ? [BoxShadow(color: color.withOpacity(0.5), blurRadius: 5)]
+                  : [],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: isSelected ? Colors.black : color,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  price,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: isSelected ? Colors.black : color,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
