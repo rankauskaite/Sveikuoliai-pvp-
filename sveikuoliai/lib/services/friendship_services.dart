beb00017a390b7580fb09e8d89b6d158173f7597
Rugile Rankauskaite <159070970+rankauskaite@users.noreply.github.com>
2025-04-05 22:29:28 +0300

    Rugile r programavimas (#29)
    * Ikelti nuotaiku paveiksliukai ir atvaizduoti programeleje
    
    * Patobulintas iprocio ekrano vizualas, itrauktas augalas ir daugiau informacijos apie iproti. Susieti naujo iprocio ir augalo sujungimai sukuriant iproti.
    
    * Pataisytas procentu paisymas kartu su progreso apskritimu.
    
    * Sukurtas augaliuku augimo funkcionalumas, iprocio progreso zymejimas
    
    * Padarytas iprocio salinimas
    
    * Padarytas tiklo vaizdavimas, uzduociu pridejimas ir vykdymas, tikslo istrynimas
    
    * Warningu patobulinimai
    
    * Prideta premium versijos reklama
    
    * Panaudotas programeles logotipas vizualiai kad butu galima atpazinti programele
    
    * Patvarkytas meditacijos ekrano vizualas
    
    * .
    
    * Vartotojo profilio informacijos atnaujinimas
    
    * Pataisyta
    
    * Profilio atnaujinimas, isskyrus nepataiso sesijos
    
    * Profilio atnaujinimas
    
    * Dienorascio ekrane parodo kuriom dienom buvo irasai, leidzia irasyti dienos irasa.
    
    * Pataisytas dienorascio rodymas
    
    * Pataisyta registracijos validacija
    
    * Pabandytas padaryti sodas
    
    Tik pabandymui idetos kelios nuotraukos ir pabandytas sukurti sodas saloje
    
    * Padarytas draugo saraso rodymas, naujo draugo pridejimas ir esamo duomenu perziura ir salinimas

--- sveikuoliai/lib/services/friendship_services.dart
+++ sveikuoliai/lib/services/friendship_services.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sveikuoliai/models/friendship_model.dart';
+import 'package:sveikuoliai/models/user_model.dart';
+import 'package:sveikuoliai/services/user_services.dart';
 
 class FriendshipService {
-  final CollectionReference friendshipCollection = FirebaseFirestore.instance.collection('friendships'); 
+  final CollectionReference friendshipCollection =
+      FirebaseFirestore.instance.collection('friendships');
+  final UserService userService = UserService();
 
   // create
   Future<void> createFriendship(Friendship friendship) async {
@@ -19,7 +23,8 @@ class FriendshipService {
   // update
   Future<void> updateFriendship(Friendship friendship) async {
     Map<String, dynamic> data = friendship.toJson();
-    data.removeWhere((key, value) => value == null); // pasalinu null values kad nesusigadintu
+    data.removeWhere((key, value) =>
+        value == null); // pasalinu null values kad nesusigadintu
     await friendshipCollection.doc(friendship.id).update(data);
   }
 
@@ -37,24 +42,60 @@ class FriendshipService {
 
   // userio visos draugystes readall
   Future<List<Friendship>> getUserFriendships(String userId) async {
-    QuerySnapshot querySnapshot = await friendshipCollection
-        .where('user1', isEqualTo: userId)
-        .get();
+    QuerySnapshot querySnapshot =
+        await friendshipCollection.where('user1', isEqualTo: userId).get();
     List<Friendship> friendships = querySnapshot.docs
-        .map((doc) => Friendship.fromJson(doc.id, doc.data() as Map<String, dynamic>))
+        .map((doc) =>
+            Friendship.fromJson(doc.id, doc.data() as Map<String, dynamic>))
         .toList();
 
-    QuerySnapshot querySnapshot2 = await friendshipCollection
-        .where('user2', isEqualTo: userId)
-        .get();
-    friendships.addAll(querySnapshot2.docs
-        .map((doc) => Friendship.fromJson(doc.id, doc.data() as Map<String, dynamic>)));
+    QuerySnapshot querySnapshot2 =
+        await friendshipCollection.where('user2', isEqualTo: userId).get();
+    friendships.addAll(querySnapshot2.docs.map((doc) =>
+        Friendship.fromJson(doc.id, doc.data() as Map<String, dynamic>)));
 
     return friendships;
   }
 
+  Future<List<FriendshipModel>> getUserFriendshipModels(String userId) async {
+    List<FriendshipModel> friendshipModels = [];
+
+    // 1. Gaunam visas draugystes kur user1 == userId
+    QuerySnapshot querySnapshot =
+        await friendshipCollection.where('user1', isEqualTo: userId).get();
+
+    // 2. Ir visas kur user2 == userId
+    QuerySnapshot querySnapshot2 =
+        await friendshipCollection.where('user2', isEqualTo: userId).get();
+
+    List<QueryDocumentSnapshot> allDocs = [
+      ...querySnapshot.docs,
+      ...querySnapshot2.docs
+    ];
+
+    for (var doc in allDocs) {
+      final data = doc.data() as Map<String, dynamic>;
+      final friendship = Friendship.fromJson(doc.id, data);
+
+      // Nustatom kuris iš jų yra draugas (ne tu)
+      String friendId =
+          friendship.user1 == userId ? friendship.user2 : friendship.user1;
+
+      // Gaunam draugo UserModel naudodami UserService
+      UserModel? friend = await userService.getUserEntry(friendId);
+
+      if (friend != null) {
+        friendshipModels
+            .add(FriendshipModel(friendship: friendship, friend: friend));
+      }
+    }
+
+    return friendshipModels;
+  }
+
   // update status - idk ar reikia
-  Future<void> updateFriendshipStatus(String user1, String user2, String newStatus) async {
+  Future<void> updateFriendshipStatus(
+      String user1, String user2, String newStatus) async {
     String friendshipId = Friendship.generateFriendshipId(user1, user2);
     await friendshipCollection.doc(friendshipId).update({'status': newStatus});
   }
