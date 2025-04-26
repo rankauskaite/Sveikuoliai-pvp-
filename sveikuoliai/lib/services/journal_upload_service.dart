4f8fbea7b9ca06d9c6f18c0e69d27dce84c0bacf
rkaluzeviciute <rugilekaluzeviciute@gmail.com>
2025-04-26 11:16:21 +0300

    korekcijos funkcijose

---
+++ sveikuoliai/lib/services/journal_upload_service.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sveikuoliai/services/drive_service.dart';
+import 'package:sveikuoliai/services/firebase_storage_service.dart';
+import 'package:sveikuoliai/services/journal_service.dart';
+import 'package:sveikuoliai/models/journal_model.dart';
+import 'package:sveikuoliai/enums/mood_enum.dart';
+
+Future<void> uploadJournalEntry() async {
+  final user = FirebaseAuth.instance.currentUser;
+  if (user == null) {
+    print('Vartotojas neprisijungęs.');
+    return;
+  }
+
+  String? photoUrl;
+
+  final providers = user.providerData.map((e) => e.providerId).toList();
+  if (providers.contains('google.com')) {
+    final fileId = await DriveService().uploadImageAndGetFileId();
+    if (fileId != null) {
+      photoUrl = 'https://drive.google.com/uc?export=view&id=$fileId';
+    }
+  } else {
+    photoUrl = await FirebaseStorageService().uploadImageAndGetUrl();
+  }
+
+  if (photoUrl == null) {
+    print('Nepavyko įkelti nuotraukos.');
+    return;
+  }
+
+  final journalEntry = JournalModel(
+    id: FirebaseFirestore.instance.collection('journal').doc().id,
+    userId: user.uid,
+    note: '', // gali keisti pagal UI
+    mood: MoodType.neutrali, // keisi pagal vartotojo pasirinkimą
+    photoUrl: photoUrl,
+    date: DateTime.now(),
+  );
+
+  await JournalService().createJournalEntry(journalEntry);
+  print('Įrašas sukurtas su nuotrauka.');
+}
