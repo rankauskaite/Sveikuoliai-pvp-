b55226bb13d59adc0764593ed306534017db5ecb
Rugile Rankauskaite <159070970+rankauskaite@users.noreply.github.com>
2025-05-04 23:14:22 +0300

    Rugile r programavimas (#42)
    * Ikelti nuotaiku paveiksliukai ir atvaizduoti programeleje
    
    * Patobulintas iprocio ekrano vizualas, itrauktas augalas ir daugiau informacijos apie iproti. Susieti naujo iprocio ir augalo sujungimai sukuriant iproti.
    
    * Pataisytas procentu paisymas kartu su progreso apskritimu.
    
    * Sukurtas augaliuku augimo funkcionalumas, iprocio progreso zymejimas
    
    * Padarytas iprocio salinimas
    
    * Padarytas tiklo vaizdavimas, uzduociu pridejimas ir vykdymas, tikslo istrynimas
    
    * Warningu patobulinimai
    
    * Prideta premium versijos reklama
    
    * Panaudotas programeles logotipas vizualiai kad butu galima atpazinti programele
    
    * Patvarkytas meditacijos ekrano vizualas
    
    * .
    
    * Vartotojo profilio informacijos atnaujinimas
    
    * Pataisyta
    
    * Profilio atnaujinimas, isskyrus nepataiso sesijos
    
    * Profilio atnaujinimas
    
    * Dienorascio ekrane parodo kuriom dienom buvo irasai, leidzia irasyti dienos irasa.
    
    * Pataisytas dienorascio rodymas
    
    * Pataisyta registracijos validacija
    
    * Pabandytas padaryti sodas
    
    Tik pabandymui idetos kelios nuotraukos ir pabandytas sukurti sodas saloje
    
    * Padarytas draugo saraso rodymas, naujo draugo pridejimas ir esamo duomenu perziura ir salinimas
    
    * Sutvarkyti prisijungimo, registracijos ir slaptazodzio priminimo ekranai
    
    * Padarytas versiju ekranas ir pritaikytas versiju pasirinkimas
    
    * Pridetas draugu tikslu pasirinkimas ekrane, ikelti ir pritaikyti paveiksliukai virs kalendoriaus
    
    * Atnaujinti augalu paveiksliukai
    
    * idk konf failai
    
    * Daug pataisymu ir papildymu
    
    Padarytas draugu tikslo ekranas, atvaizduoti tikslai su draugais, padarytas tikslu pridejimas su draugais, ikeltos reklamos ir salos, reklamos atvaizduotos, pakeistas iprociu progreso zymejimo UI, pakeistas iprociu ir tikslu redagavimas padarytas tik custom iprociam ar tikslam, draugu tikslo salinimas, custom redagavimas ir uzduociu pridejimas ir vykdymas.
    
    * Prideta bendruomenes versija, padarytas tikslo uzduoties redagavimas ir salinimas
    
    * iskelti bendrai naudojami kodo fragmentai i atskirus failus siekiant sumazinti kodo dubliavima ir ilgi
    
    * Pataisyti notifications ir ikelti vizualai.
    
    Pataisytas zinuciu siuntimas.
    Ikelti nauji paveiksliukai:
      dienorascio dienos vizualas
      mano sodo vizualas, padarytas kaip mygtukas ir premium versijos gif irgi padarytas kaip mugtukas.
    Ikeltas ir pritaikytas naujas veikiantis meditacijos vizualas.
    
    * Panaikinta bendruomenes versija
    
    * Galima prideti tik tuos draugus, kurie turi premium versija
    
    * Padaryti paprasto tikslo ir draugu tikslo uzduociu pakeitimai, sukurtos default uzduotys
    
    * Patobulintas draugo pridejimas, salinant drauga pasalina ir bendrus tikslus
    
    * Padarytas bendru tikslu vizualas ir uzduociu logika
    
    * Warningu pasalinimas
    
    * Padaryti iprociu, tikslu ir draugu sodai

--- sveikuoliai/lib/screens/garden.dart
+++ sveikuoliai/lib/screens/garden.dart
@@ -1,7 +1,15 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:sveikuoliai/models/goal_model.dart';
+import 'package:sveikuoliai/models/habit_model.dart';
+import 'package:sveikuoliai/models/shared_goal_model.dart';
+import 'package:sveikuoliai/services/auth_services.dart';
+import 'package:sveikuoliai/services/goal_services.dart';
+import 'package:sveikuoliai/services/habit_services.dart';
+import 'package:sveikuoliai/services/plant_image_services.dart';
+import 'package:sveikuoliai/services/shared_goal_services.dart';
 import 'package:sveikuoliai/widgets/bottom_navigation.dart';
+import 'package:sveikuoliai/widgets/custom_snack_bar.dart';
 
 class GardenScreen extends StatefulWidget {
   const GardenScreen({super.key});
@@ -11,27 +19,172 @@ class GardenScreen extends StatefulWidget {
 }
 
 class _GardenScreenState extends State<GardenScreen> {
+  final AuthService _authService = AuthService();
+  List<Map<String, dynamic>> userHabits = [];
+  List<Map<String, dynamic>> userGoals = [];
+  List<Map<String, dynamic>> userSharedGoals = [];
+  final HabitService _habitService = HabitService();
+  final GoalService _goalService = GoalService();
+  final SharedGoalService _sharedGoalService = SharedGoalService();
+  final PageController _pageController = PageController();
   final Random _random = Random();
-  final int plantCount = 5; // Nustatomas augaliukų skaičius
+  String userUsername = '';
+  int _currentPage = 0;
+  final int plantCount = 5;
+  List<String> count = ['0', '0', '0'];
 
-  List<Positioned> _generatePlants() {
-    List<Positioned> plants = [];
-    for (int i = 0; i < plantCount; i++) {
-      double left = -5 + _random.nextDouble() * 200; // X koordinatė
-      double top = 15 + _random.nextDouble() * 20; // Y koordinatė
-      plants.add(
-        Positioned(
-          left: left,
-          top: top,
-          child: Image.asset(
-            'assets/images/augalai/saulegraza/12.png',
-            width: 100,
-            height: 100,
-          ),
+  final List<String> titlesTop = ['Įpročių', 'Tikslų', 'Draugų'];
+  final List<String> titlesBottom = ['sodas', 'sodas', 'sodas'];
+  final List<String> subtitles = [
+    'Šiuo metu bandai\nišsiugdyti\nįpročius',
+    'Šiuo metu vykdai\ntikslus',
+    'Šiuo metu su draugais\nvykdai tikslus'
+  ];
+  final List<String> images = [
+    'assets/images/salos/sala_iprociu.png',
+    'assets/images/salos/sala_tikslu.png',
+    'assets/images/salos/sala_draugu.png',
+  ];
+  final List<Color> backgroundColors = [
+    Color(0xFFB388EB), // Spalva pirmam puslapiui
+    Color(0xFF27B1D2), // Spalva antram puslapiui
+    Color(0xFF5A741E), // Spalva trečiam puslapiui
+  ];
+
+  List<Positioned> _generatePlants(List<Map<String, dynamic>> sourceList) {
+    List<Map<String, dynamic>> plantData = [];
+    List<Offset> usedPositions = [];
+    int maxAttempts = 100;
+
+    while (plantData.length < sourceList.length && maxAttempts > 0) {
+      double left = -5 + _random.nextDouble() * 200;
+      double top = 18 + _random.nextDouble() * 28;
+      Offset newPos = Offset(left, top);
+
+      bool tooClose = usedPositions.any(
+        (pos) => (pos - newPos).dx.abs() < 40 && (pos - newPos).dy.abs() < 5,
+      );
+
+      if (!tooClose) {
+        usedPositions.add(newPos);
+        plantData.add({'left': left, 'top': top});
+      }
+
+      maxAttempts--;
+    }
+
+    plantData.sort((a, b) => a['top'].compareTo(b['top']));
+
+    return List.generate(sourceList.length, (index) {
+      final plant = sourceList[index];
+      final double left = plantData[index]['left'];
+      final double top = plantData[index]['top'];
+      final String imagePath = PlantImageService.getPlantImage(
+        plant['plantId'],
+        plant['points'],
+      );
+
+      return Positioned(
+        left: left,
+        top: top,
+        child: Image.asset(
+          imagePath,
+          width: 100,
+          height: 100,
         ),
       );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  // Funkcija, kad gauti prisijungusio vartotojo duomenis
+  Future<void> _fetchUserData() async {
+    try {
+      Map<String, String?> sessionData = await _authService.getSessionUser();
+      setState(
+        () {
+          userUsername = sessionData['username'] ?? "Nežinomas";
+        },
+      );
+      await _fetchUserHabits(userUsername);
+      await _fetchUserGoals(userUsername);
+      await _fetchUserSharedGoals(userUsername);
+      // Atnaujinti count kai visi duomenys jau gauti
+      setState(() {
+        count = [
+          userHabits.length.toString(),
+          userGoals.length.toString(),
+          userSharedGoals.length.toString(),
+        ];
+      });
+    } catch (e) {
+      String message = 'Klaida gaunant duomenis ❌';
+      showCustomSnackBar(context, message, false);
     }
-    return plants;
+  }
+
+  Future<void> _fetchUserHabits(String username) async {
+    try {
+      // Gaukime vartotojo įpročius
+      List<HabitInformation> habits =
+          await _habitService.getUserHabits(username);
+
+      // Atnaujiname būsena su naujais duomenimis
+      setState(() {
+        userHabits = habits
+            .map((habit) => {
+                  'plantId': habit.habitModel.plantId,
+                  'points': habit.habitModel.points
+                })
+            .toList();
+      });
+    } catch (e) {
+      showCustomSnackBar(context, 'Klaida kraunant įpročius ❌', false);
+    }
+  }
+
+  Future<void> _fetchUserGoals(String username) async {
+    try {
+      // Gaukime vartotojo įpročius
+      List<GoalInformation> goals = await _goalService.getUserGoals(username);
+
+      // Atnaujiname būsena su naujais duomenimis
+      setState(() {
+        userGoals = goals
+            .map((goal) => {
+                  'plantId': goal.goalModel.plantId,
+                  'points': goal.goalModel.points
+                })
+            .toList();
+      });
+    } catch (e) {
+      showCustomSnackBar(context, 'Klaida kraunant tikslus ❌', false);
+    }
+  }
+
+  Future<void> _fetchUserSharedGoals(String username) async {
+    try {
+      // Gaukime vartotojo įpročius
+      List<SharedGoalInformation> goals =
+          await _sharedGoalService.getSharedUserGoals(username);
+
+      // Atnaujiname būsena su naujais duomenimis
+      setState(() {
+        userSharedGoals = goals
+            .map((goal) => {
+                  'plantId': goal.sharedGoalModel.plantId,
+                  'points': goal.sharedGoalModel.points
+                })
+            .toList();
+      });
+    } catch (e) {
+      showCustomSnackBar(context, 'Klaida kraunant draugų tikslus ❌', false);
+    }
   }
 
   @override
@@ -53,9 +206,9 @@ class _GardenScreenState extends State<GardenScreen> {
                   width: 320,
                   height: 600,
                   decoration: BoxDecoration(
-                    color: Colors.white,
+                    color: Color(0xFFE7EDD9),
                     borderRadius: BorderRadius.circular(30),
-                    border: Border.all(color: Colors.white, width: 20),
+                    border: Border.all(color: Color(0xFFE7EDD9), width: 20),
                   ),
                   child: Column(
                     children: [
@@ -70,25 +223,148 @@ class _GardenScreenState extends State<GardenScreen> {
                           ),
                         ],
                       ),
-                      const SizedBox(height: 20),
-                      Text(
-                        'Sodas',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+                      const SizedBox(height: 10),
+                      Container(
+                        width: 250, // fiksuotas plotis
+                        height: 120, // fiksuotas aukštis
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFC2DD84),
+                          borderRadius: BorderRadius.circular(15),
                         ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: titlesTop[_currentPage] + '\n',
+                                    style: TextStyle(
+                                      fontSize: 45,
+                                      fontWeight: FontWeight.bold,
+                                      color: backgroundColors[_currentPage],
+                                      height:
+                                          1.2, // sumažintas tarpas tarp eilučių
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: titlesBottom[_currentPage],
+                                    style: const TextStyle(
+                                      fontSize: 45,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      height:
+                                          1.0, // dar mažesnis tarpas šiai eilutei, jei norisi
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              subtitles[_currentPage],
+                              textAlign: TextAlign.right,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFC2DD84),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              count[_currentPage],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 35,
+                                color: backgroundColors[_currentPage],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                        ],
                       ),
                       const SizedBox(height: 30),
                       Stack(
                         children: [
-                          Image.asset(
-                            'assets/images/salos/sala_tikslu.png',
-                            width: 300,
-                            height: 220,
+                          // Kiti Stack elementai (pvz., fonas)
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: SizedBox(
+                              width: 300, // page view plotis
+                              height: 250, // page view aukštis
+                              child: PageView.builder(
+                                controller: _pageController,
+                                itemCount: 3,
+                                onPageChanged: (index) {
+                                  setState(() {
+                                    _currentPage = index;
+                                  });
+                                },
+                                itemBuilder: (context, index) {
+                                  List<Map<String, dynamic>> currentData;
+                                  if (index == 0) {
+                                    currentData = userHabits;
+                                  } else if (index == 1) {
+                                    currentData = userGoals;
+                                  } else {
+                                    currentData = userSharedGoals;
+                                  }
+
+                                  return Stack(
+                                    alignment: Alignment.bottomCenter,
+                                    children: [
+                                      Image.asset(
+                                        images[index],
+                                        width: 300,
+                                        height: 200,
+                                      ),
+                                      ..._generatePlants(currentData),
+                                    ],
+                                  );
+                                },
+                              ),
+                            ),
                           ),
-                          ..._generatePlants(),
                         ],
                       ),
+
+                      //const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(3, (index) {
+                          final isActive = _currentPage == index;
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            child: Icon(
+                              isActive
+                                  ? Icons.circle
+                                  : Icons.circle, // Naudoti tą patį ikoną
+                              size: 12,
+                              color: isActive
+                                  ? Color(0xFF8093F1)
+                                  : Colors
+                                      .grey[400], // Pilka spalva neaktyviems
+                              semanticLabel: 'Neaktyvus', // Pagal poreikį
+                            ),
+                          );
+                        }),
+                      ),
+                      const SizedBox(height: 10),
                     ],
                   ),
                 ),
